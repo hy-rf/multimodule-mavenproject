18f35b72f0f39762b5ce00703b4e534c21381a5d
hy-rf <heyu880202@gmail.com>
2025-06-09 16:44:52 +0800

    chore: comment out unused dao config file

--- dao/src/main/java/com/mysbproject/dao/config/DaoConfig.java
+++ dao/src/main/java/com/mysbproject/dao/config/DaoConfig.java
@@ -1,11 +1,11 @@
-package com.mysbproject.dao.config;
+// package com.mysbproject.dao.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@Configuration
-@EnableJpaRepositories(basePackages = "com.mysbproject")
-@EntityScan(basePackages = "com.mysbproject.dao")
-public class DaoConfig {
-}
+// @Configuration
+// @EnableJpaRepositories(basePackages = "com.mysbproject")
+// @EntityScan(basePackages = "com.mysbproject.dao")
+// public class DaoConfig {
+// }
