055f631298e3e726bc9818faec46df3246ed2c70
hy-rf <heyu880202@gmail.com>
2025-06-12 16:13:47 +0800

    fix: restore contextLoads test in MyServiceTest for validation of MyService functionality

--- core/src/test/java/com/mysbproject/service/MyServiceTest.java
+++ core/src/test/java/com/mysbproject/service/MyServiceTest.java
@@ -16,10 +16,10 @@ public class MyServiceTest {
   @Autowired
   private MyService myService;
 
-  // @Test
-  // public void contextLoads() {
-  // assertThat(myService.message()).isNotNull();
-  // }
+  @Test
+  public void contextLoads() {
+    assertThat(myService.message()).isNotNull();
+  }
 
   @SpringBootApplication(scanBasePackages = "com.mysbproject")
   static class TestConfiguration {
