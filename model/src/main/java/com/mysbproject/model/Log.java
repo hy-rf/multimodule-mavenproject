9a6f34c66f24e6935fd104a82119722000197eaf
hy-rf <heyu880202@gmail.com>
2025-06-16 21:12:12 +0800

    chore: add commented logs table model

---
+++ model/src/main/java/com/mysbproject/model/Log.java
@@ -0,0 +1,17 @@
+// package com.mysbproject.model;
+
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+// @Entity
+// @Table(name = "logs")
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Log {
+// private Long id;
+// private String name;
+// }
