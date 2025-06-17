2128d5a1b8deb82e5a1162423d33b7cddcdde6ff
hy-rf <heyu880202@gmail.com>
2025-06-18 06:31:43 +0800

    chore: add entities for school management system

---
+++ model/src/main/java/com/mysbproject/model/Class.java
@@ -0,0 +1,33 @@
+package com.mysbproject.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "classes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Class {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 100, nullable = false)
+  private String name;
+
+  @ManyToOne
+  @JoinColumn(name = "grade_id", nullable = false)
+  private Grade grade;
+}
---
+++ model/src/main/java/com/mysbproject/model/Teacher.java
@@ -0,0 +1,29 @@
+package com.mysbproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "teachers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+}
