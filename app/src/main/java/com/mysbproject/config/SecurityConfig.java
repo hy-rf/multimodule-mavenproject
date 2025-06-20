ce25a60adf4d4a1788481063b77b92e82d3926ec
hy-rf <heyu880202@gmail.com>
2025-06-20 14:01:33 +0800

    feat: enhance SecurityConfig by adding environment profile check for CSRF

--- app/src/main/java/com/mysbproject/config/SecurityConfig.java
+++ app/src/main/java/com/mysbproject/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mysbproject.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,15 +19,21 @@ public class SecurityConfig {
   @Autowired
   private AuthorizeFilter authorizeFilter;
 
+  @Autowired
+  private Environment env;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    boolean isDev = java.util.Arrays.asList(env.getActiveProfiles()).contains("dev");
     http
-        .csrf(csrf -> csrf.disable())
+        .csrf(csrf -> {
+          if (isDev) {
+            csrf.disable();
+          }
+        })
         .formLogin(form -> form.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> auth
-            // .requestMatchers("/", "/login", "/register", "/refresh", "/home").permitAll()
-            .anyRequest().permitAll());
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
     http.addFilterBefore(authorizeFilter,
         UsernamePasswordAuthenticationFilter.class);
     return http.build();
