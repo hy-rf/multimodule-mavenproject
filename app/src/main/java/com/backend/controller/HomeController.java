b63414bd600723fbc6408a0d5cd7ccd6f856e6b3
hy-rf <heyu880202@gmail.com>
2025-06-21 09:36:08 +0800

    feat: add springdoc-openapi dependency for API documentation and remove outdated version

--- app/src/main/java/com/backend/controller/HomeController.java
+++ app/src/main/java/com/backend/controller/HomeController.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
--- app/src/main/java/com/backend/security/AuthorizeFilter.java
+++ app/src/main/java/com/backend/security/AuthorizeFilter.java
@@ -1,7 +1,6 @@
 package com.backend.security;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
