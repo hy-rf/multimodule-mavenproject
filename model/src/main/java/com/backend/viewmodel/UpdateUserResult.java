51daaaa354dc48b13afda9c26eb9229b89dbfb66
hy-rf <heyu880202@gmail.com>
2025-06-23 08:54:08 +0800

    feat: add getter for message field in UpdateUserResult class

--- model/src/main/java/com/backend/viewmodel/UpdateUserResult.java
+++ model/src/main/java/com/backend/viewmodel/UpdateUserResult.java
@@ -1,8 +1,10 @@
 package com.backend.viewmodel;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class UpdateUserResult {
   private String message;
 }
