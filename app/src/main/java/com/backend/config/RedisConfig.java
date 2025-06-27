9e9549ce9a7adf53966fe2e217cb9a769f7d99c6
hy-rf <heyu880202@gmail.com>
2025-06-27 18:35:47 +0800

    chore: add redis config

---
+++ app/src/main/java/com/backend/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package com.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories(basePackages = "com.backend.repository.cache")
+public class RedisConfig {
+}
