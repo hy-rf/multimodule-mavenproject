c6457d6fbacec8ac548a636ee44320bea1b5a2c6
hy-rf <heyu880202@gmail.com>
2025-06-28 16:20:27 +0800

    chore: remove unused import in HomeController

--- app/src/main/java/com/backend/controller/HomeController.java
+++ app/src/main/java/com/backend/controller/HomeController.java
@@ -3,8 +3,6 @@ package com.backend.controller;
 import jakarta.servlet.http.HttpServletResponse;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
