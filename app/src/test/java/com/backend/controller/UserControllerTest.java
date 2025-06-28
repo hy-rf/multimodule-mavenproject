4e0deba397698674df5ce0c316b1615697b96096
hy-rf <heyu880202@gmail.com>
2025-06-28 17:14:12 +0800

    chore: remove unused tests in UserControllerTest

--- app/src/test/java/com/backend/controller/UserControllerTest.java
+++ app/src/test/java/com/backend/controller/UserControllerTest.java
@@ -1,33 +1,4 @@
 package com.backend.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.backend.common.JwtUtils;
-import com.backend.service.CustomUserDetailsService;
-
-@ActiveProfiles("test")
-@WebMvcTest(UserControllerTest.class)
 public class UserControllerTest {
-  @Autowired
-  private MockMvc mockMvc;
-
-  @MockitoBean
-  private JwtUtils jwtUtils;
-
-  @MockitoBean
-  private CustomUserDetailsService customUserDetailsService;
-
-  @Test
-  @WithMockUser(roles = "admin")
-  public void testUsersEndpoint() throws Exception {
-    mockMvc.perform(get("/users"));
-  }
 }
