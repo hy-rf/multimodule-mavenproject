5e2283320865631a77d4589f7e96542400bf0f61
hy-rf <heyu880202@gmail.com>
2025-06-28 14:02:13 +0800

    chore: add LoginAttemptRepository

---
+++ repository/src/main/java/com/backend/repository/cache/LoginAttemptRepository.java
@@ -0,0 +1,6 @@
+package com.backend.repository.cache;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LoginAttemptRepository extends CrudRepository<Integer, Integer> {
+}
