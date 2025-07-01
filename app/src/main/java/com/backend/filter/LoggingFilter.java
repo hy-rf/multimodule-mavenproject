1a2882839e6273e0fb946924d4e339c9dce7afb5
hy-rf <heyu880202@gmail.com>
2025-07-02 00:17:35 +0800

    chore: remove unused Logger imports from LoggingFilter

--- app/src/main/java/com/backend/filter/LoggingFilter.java
+++ app/src/main/java/com/backend/filter/LoggingFilter.java
@@ -1,8 +1,6 @@
 package com.backend.filter;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.Filter;
